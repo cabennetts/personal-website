687af2eeedc7156a9e5a1cdc2f4bc22da435d399
Caleb <48822049+cabennetts@users.noreply.github.com>
2023-05-24 19:17:05 -0500

    fix build error

--- app/other/page.tsx
+++ app/other/page.tsx
@@ -8,7 +8,7 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useRef } from "react";
 import { useLastViewedPhoto } from '../../utils/useLastViewedPhoto'
-import { getStaticProps } from "../api/about/route";
+import { getStaticProps } from "../api/other/route";
 import ImageCard from "@/components/ImageCard/ImageCard";
 
 export const metadata: Metadata = {
