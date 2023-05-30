86162856db80ae36624b4f7e35512a7044139673
Caleb <48822049+cabennetts@users.noreply.github.com>
2023-05-30 16:37:09 -0500

    updated git ignore

---
+++ app/api/other/route.ts
@@ -0,0 +1,29 @@
+// import cloudinary from "@/utils/cloudinary"
+// import type { ImageProps } from "@/utils/types"
+
+// export async function GET() {
+//     const results = await cloudinary.v2.search
+//       .expression(`folder:${process.env.CLOUDINARY_FOLDER}/*`)
+//       .sort_by('public_id', 'desc')
+//       .max_results(400)
+//       .execute()
+//     let reducedResults: ImageProps[] = []
+  
+//     let i = 0
+//     for (let result of results.resources) {
+//       reducedResults.push({
+//         id: i,
+//         height: result.height,
+//         width: result.width,
+//         public_id: result.public_id,
+//         format: result.format,
+//       })
+//       i++
+//     }
+  
+//     return {
+//       props: {
+//         images: reducedResults,
+//       },
+//     }
+//   }
--- lib/getUnsplashImages.ts
+++ lib/getUnsplashImages.ts
@@ -1,7 +1,13 @@
 export default async function getAllImages() {
 
-    const res = await fetch(`https://api.unsplash.com/users/cabennetts/photos?client_id=${process.env.UNSPLASH_ACCESS_KEY}&per_page=30&order_by=popular`)
-
+    const res = await fetch(`https://api.unsplash.com/users/cabennetts/photos?client_id=${process.env.UNSPLASH_ACCESS_KEY}&per_page=30&order_by=popularage=1`)
+    // const res2 = await fetch(`https://api.unsplash.com/users/cabennetts/photos?client_id=${process.env.UNSPLASH_ACCESS_KEY}&per_page=30&order_by=popularage=2`)
+    
     if(!res.ok) throw new Error('failed to fetch data')
+    // if(!res.ok || !res2.ok) throw new Error('failed to fetch data')
+    
+    // res.json()
+    // res2.json()
+    
     return res.json()
 }
