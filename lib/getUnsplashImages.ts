082f5ab428a96c5ba5cc47fe23a981ed5d2aa62b
Caleb <48822049+cabennetts@users.noreply.github.com>
2023-06-02 14:25:43 -0500

    commented out all notion requests to ensure build

--- lib/getUnsplashImages.ts
+++ lib/getUnsplashImages.ts
@@ -1,6 +1,6 @@
 export default async function getAllImages() {
 
-    const res = await fetch(`https://api.unsplash.com/users/cabennetts/photos?client_id=${process.env.UNSPLASH_ACCESS_KEY}&per_page=30&order_by=popular&page=1`)
+    const res = await fetch(`https://api.unsplash.com/users/cabennetts/photos?client_id=${process.env.UNSPLASH_ACCESS_KEY}&per_page=30&order_by=popular&page=2`)
     // const res2 = await fetch(`https://api.unsplash.com/users/cabennetts/photos?client_id=${process.env.UNSPLASH_ACCESS_KEY}&per_page=30&order_by=popular&page=2`)
     
     if(!res.ok) throw new Error('failed to fetch data')
--- lib/notion.ts
+++ lib/notion.ts
@@ -1,26 +1,26 @@
 
 import React from "react";
 import { Client } from '@notionhq/client'
-import { BlockObjectResponse, PageObjectResponse } from "@notionhq/client/build/src/api-endpoints";
+// import { BlockObjectResponse, PageObjectResponse } from "@notionhq/client/build/src/api-endpoints";
 
 export const notion = new Client({
     auth: process.env.NOTION_SECRET_KEY,
 });
 
-export async function fetchPages() {
-    const res = await notion.databases.query({
-        database_id: process.env.NOTION_DATABASE_ID!,
-        // filter: {
-        //     property: "Status",
-        //     select: {
-        //         equals: "Published",
-        //     }
-        // }
-    })
+// export async function fetchPages() {
+//     const res = await notion.databases.query({
+//         database_id: process.env.NOTION_DATABASE_ID!,
+//         // filter: {
+//         //     property: "Status",
+//         //     select: {
+//         //         equals: "Published",
+//         //     }
+//         // }
+//     })
     
-    console.log(res)
-    return res.results
-}
+//     console.log(res)
+//     return res.results
+// }
 // export const fetchPages = React.cache(() => {
 //     return notion.databases.query({
 //         database_id: process.env.NOTION_DATABASE_ID!,
