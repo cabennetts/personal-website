c990b45ceb58365ebac15406cd6f44328015efce
Caleb <48822049+cabennetts@users.noreply.github.com>
2023-06-30 12:31:07 -0500

    add shadcnui components

---
+++ constants/index.ts
@@ -0,0 +1,54 @@
+export const navBarLinks = [
+    {
+        title: "New Clients",
+        links: [
+            { title: "Requirements", url: "/new-clients/requirements"},
+            { title: "Daycare & Boarding Agreement", url: "https://pawloyalty.formstack.com/forms/fetchersplaystay" },
+            { title: "Schedule a Tour", url: "/new-clients/schedule-tour" },
+        ],
+    },
+    {
+        title: "Services",
+        links: [
+            { title: "Dog Daycare", url: "/services/dog-daycare"},
+            { title: "Overnight Boarding", url: "/services/overnight-boarding" },
+            { title: "Professional Grooming & Spa", url: "/services/grooming-and-spa" },
+            { title: "Current Client Reservations", url: "https://app.pawloyalty.com/ReservationWidget/BookingWidget/fa37b076-4013-4984-8039-4aa1334ebb92/3" },
+        ],
+    },
+    {
+        title: "About",
+        links: [
+            { title: "About", url: "/about" },
+            { title: "Meet the Staff", url: "/about/meet-the-staff" },
+        ],
+    },
+    {
+        title: "Fetch Cam",
+        links: [
+            { title: "Public Cameras", url: "https://onlinedoggy.com/clients/clientcp/ViewerHtml5.asp?id=%7B2FC8C909-AB85-4986-87D5-A36EDD326FE3%7D&source=" },
+            { title: "Private Penthouse", url: "https://onlinedoggy.com/clients/clientcp/ViewerHTML5-p.asp?id=%7B2FC8C909-AB85-4986-87D5-A36EDD326FE3%7D&source=" },
+        ],
+    },
+    {
+        title: "Company",
+        links: [
+            { title: "Testimonials", url:"/company/testimonials" },
+            { title: "Photos", url: "/company/photos" },
+            { title: "Blog", url: "/company/blog" },
+            { title: "FAQ", url: "/company/faq" },
+            { title: "Contact", url: "/company/contact" },
+        ],
+    },
+    {
+        title: "Forms",
+        links: [
+            { title: "Daycare & Boarding Agreement", url: "https://pawloyalty.formstack.com/forms/fetchersplaystay" },
+            { title: "Current Client Reservations", url: "https://app.pawloyalty.com/ReservationWidget/BookingWidget/fa37b076-4013-4984-8039-4aa1334ebb92/3" },
+            { title: "Senior Waiver", url: "https://pawloyalty.formstack.com/forms/fetchers_seniorwaiver" },
+            { title: "Grooming Application", url:"https://pawloyalty.formstack.com/forms/fetchersplaystaygrooming"},
+            { title: "Boarding Check-in Details", url: "https://pawloyalty.formstack.com/forms/boarding_checkin" },
+            { title: "Penthouse Contract", url: "https://pawloyalty.formstack.com/forms/fetchers_penthousewaiver" },
+        ],
+    },
+];
